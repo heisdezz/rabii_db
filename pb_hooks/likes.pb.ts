69d5cbab2ca6ac6dc7f449fc9567a7ebefd63628
destiny <desto4q@gmail.com>
2026-03-21 01:17:00 +0100

    likes_count and dislikes count, unlike

--- pb_hooks/likes.pb.ts
+++ pb_hooks/likes.pb.ts
@@ -59,7 +59,67 @@ routerAdd(
   },
   $apis.requireAuth(),
 );
+routerAdd(
+  "POST",
+  "/dislike/{id}",
+  (e) => {
+    const video_id = e.request.pathValue("id");
+    const info = e.requestInfo();
+    const user_id = info.auth?.id;
+    const gen_id = `${user_id}${video_id}`;
+    try {
+      const pr_col = e.app.findCollectionByNameOrId("post_reactions");
+      const new_like = new Record(pr_col);
+      new_like.set("id", gen_id);
+      new_like.set("like_dislike", 0);
+      new_like.set("user", user_id);
+      new_like.set("post", video_id);
+      e.app.save(new_like);
+      return e.json(201, {
+        data: gen_id,
+        message: "video liked",
+      });
+    } catch (err) {
+      console.log("failed to like video:", err);
+      return e.json(500, {
+        data: null,
+        message: "internal server error",
+      });
+    }
+  },
+  $apis.requireAuth(),
+);
 
+routerAdd(
+  "POST",
+  "/remove_like/{id}",
+  (e) => {
+    const video_id = e.request.pathValue("id");
+    const info = e.requestInfo();
+    const user_id = info.auth?.id;
+    const gen_id = `${user_id}${video_id}`;
+    try {
+      const pr_col = e.app.findCollectionByNameOrId("post_reactions");
+      const new_like = new Record(pr_col);
+      new_like.set("id", gen_id);
+      new_like.set("like_dislike", 1);
+      new_like.set("user", user_id);
+      new_like.set("post", video_id);
+      e.app.save(new_like);
+      return e.json(201, {
+        data: gen_id,
+        message: "video liked",
+      });
+    } catch (err) {
+      console.log("failed to like video:", err);
+      return e.json(500, {
+        data: null,
+        message: "internal server error",
+      });
+    }
+  },
+  $apis.requireAuth(),
+);
 onRecordAfterCreateSuccess((e) => {
   const number = e.record?.get("like_dislike");
   const post_id = e.record?.get("post");
